8ae9a22295eff5c00754e82fb2324beb21034b03
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-01-24 23:23:33 -0300

    feat : Added Navigator URL Projects

--- lib/Models/my_strings.dart
+++ lib/Models/my_strings.dart
@@ -18,13 +18,14 @@ class MyStrings {
   static final String myTagLine = "Mobile Developer";
   static final String myDescription =
       "Enthusiastic about the technological area related to programming and its entire ecosystem, I started my training with studies on native applications using Java and Koltin, as I have always identified myself with mobile development. After that I entered the world of hybrid development, perfecting myself in Flutter, as well as in Android Native, my main stack currently. My passion for programming and the IT area started in childhood, and since then I've been studying and working continuously to become a better professional.";
-  static final String myMail = "devshakib015@gmail.com";
-  static final String myPhone = "+8801710265421";
-  static final String myMessenger = "venomShakib";
-  static final String myFacebook = "venomShakib";
-  static final String myWhatsApp = "+8801710265421";
-  static final String myWechat = "venomshakib";
+
   static final String myGithub = "Franciscof11";
+  static final String weatherNowLink =
+      "https://github.com/Franciscof11/WeatherNow--Flutter";
+  static final String mockShopLink =
+      "https://github.com/Franciscof11/Mock-Shop--Flutter";
+  static final String portfolioLink =
+      "https://github.com/Franciscof11/Portfolio--Flutter";
   static final String myResume =
       "https://docs.google.com/document/d/1Ui_bfssTp0Yqb4X7-5rWfvpfEGLX5_0I-w6AWGxBiTk/edit?usp=share_link";
 
